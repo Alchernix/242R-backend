c32efe10d8dff445ae33fe29f347b08eb3910c6f
Alchernix <alchernix149@naver.com>
2024-11-19 22:49:50 +0900

    5주차 과제

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const express = require('express');
-const port = 3000;
-const app = express();
-app.set('views', `${__dirname}/views`);
-app.set('view engine', 'pug');
-app.get('/', (req, res) => res.render('index.pug'));
-app.get('/page', (req, res) => {
-    const { page } = req.query;
-    res.render('board.pug', { page });
-});
-app.get('/posts', (req, res) => {
-    const { until } = req.query;
-    const untilParsed = parseInt(until, 10);
+const { runQuery } = require("./database")
+const getScoreStats = async () => {
+    const sql = 'SELECT course, Count(*) AS cnt, Avg(final) AS avg, ' +
+        'Stddev(final) AS stddev FROM scores GROUP BY course'
+    const results = await runQuery(sql);
+    return results;
+}
 
-    const posts = [];
-    if (!isNaN(untilParsed)) {
-        for (let i = 0; i < untilParsed; i++) {
-            posts.push(`post ${i + 1}`);
-        }
-    }
-    res.render('posts.pug', { posts })
-});
-app.listen(port, () => console.log(`Server listeningonport${port}!`)); { }
+const getScoreByIdName = async (id, name) => {
+    const sql = `SELECT * FROM scores WHERE id = ${id} AND student = '${name}'`
+    const results = await runQuery(sql)
+    return results[0]
+}
+
+const createScore = async (name, course, midterm, final) => {
+    const sql = `INSERT INTO scores VALUES (DEFAULT, '${name}', '${course}', '${midterm}', '${final}')`
+    const result = await runQuery(sql)
+    return result
+}
+
+(async () => {
+    const stats = await getScoreStats() //select로 받아온 모든 값이 배열로 저장
+    stats.forEach(stat => {
+        const { course, cnt, avg, stddev } = stat
+        console.log(`course: ${course}, cnt: ${cnt}, avg: ${avg}, stddev: ${stddev}`)
+    })
+
+    const scoreData = await getScoreByIdName(2, "Joe")
+    const { course, final } = scoreData;
+    console.log(course, final)
+
+    console.dir(await createScore('Alchernix', 'Programming Language', 100, 100))
+    console.dir(await getScoreByIdName(9, "Alchernix"))
+})()
